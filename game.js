2bdc61b026f54e449bb14ef39838b90e1607ac6c
ChaseObserves <30663791+ChaseObserves@users.noreply.github.com>
2018-01-04 14:58:58 -0700

    Delete game.js

--- game.js
+++
@@ -1,6 +0,0 @@
-var words = ["cat", "dog", "mouse", "horse", "monkey", "bear", "lion", "tiger", "lemur", "koala", "hamster", "goat", "fox", "rabbit", "snake", "badger", "owl", "seagull", "ostrich", "chameleon"];
-
-exports.chooseWord = function(){
-	var randNum = Math.floor((Math.random()*words.length)+1);
-	return words[randNum];
-}
